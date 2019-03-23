ca36e35d497adf5739618f57ef3693110a5d51bf
Worapol Buraphan <xeastcrast@hotmail.com>
2019-03-23 16:20:28 +0700

    cover blur when hover

--- src/components/Timeline/Cover/index.js
+++ src/components/Timeline/Cover/index.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { usePreload } from "helpers/util";
-import './styles.scss';
+import Dragable from "components/Dragable";
+import { Icon } from "antd";
+import classnames from "classnames";
+import "./styles.scss";
+import { animationFrameScheduler } from "rxjs";
+import { AnimationFrameScheduler } from "rxjs/internal/scheduler/AnimationFrameScheduler";
 
 function CoverImage({ src }) {
   const [imagePath, setImagePath] = useState(null);
+  const coverRef = useRef();
+  const imgRef = useRef();
+  const [bluring, setBluring] = useState(true);
   useEffect(() => {
     usePreload(src)
       .then(result => {
@@ -11,10 +19,45 @@ function CoverImage({ src }) {
       })
       .catch(error => console.log(error, "Cover image can't loaded"));
   }, []);
+  function onDrag(e) {
+    const { clientHeight, clientWidth } = coverRef.current;
+    const { clientY } = e;
+    const moveY = 100 - (clientY * 100) / clientHeight;
+    console.log("percent", moveY);
+    imgRef.current.style.objectPosition = `center ${moveY}%`;
+    // console.log("draging", clientHeight, clientWidth);
+  }
+  function onDragEnd() {
+    console.log("drag end");
+  }
   return (
-    <div className="coverImageWrapper">
-      {imagePath && <img src={imagePath} />}
-    </div>
+    <Dragable
+      active={false}
+      dragRef={coverRef}
+      onDrag={onDrag}
+      onDragEnd={onDragEnd}
+    >
+      <div className="coverImageWrapper">
+        <div className="coverAction">
+          <div className="actionButton">
+            <button>
+              <Icon type="picture" />
+            </button>
+            <button onClick={moveImage}>
+              <Icon type="drag" />
+            </button>
+          </div>
+        </div>
+        {imagePath && (
+          <img
+            className={classnames({ bluring })}
+            ref={imgRef}
+            draggable={false}
+            src={imagePath}
+          />
+        )}
+      </div>
+    </Dragable>
   );
 }
 
